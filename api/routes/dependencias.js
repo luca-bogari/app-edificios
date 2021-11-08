f9e5532abb795016ce9b68d31a86c7d6de3ed4f0
luca-bogari <lucabogarimassina@gmail.com>
2021-11-07 23:14:55 -0300

    rutas backend terminadas

--- api/routes/dependencias.js
+++ api/routes/dependencias.js
@@ -2,12 +2,27 @@ const express = require("express");
 const depenRouter = express.Router();
 const { Edificios, Dependencias } = require("../models");
 
+// traer todas las dependencias de un edificio por el id
 depenRouter.get("/:id", (req, res, next) => {
   Dependencias.findAll({ where: { EdificioId: req.params.id } })
     .then(dependencias => {
       res.status(200).json(dependencias);
     })
-    .catch(next);
+    .catch(err => {
+      next(err);
+    });
+});
+
+// agregar una nueva dependencia
+depenRouter.put("/:id", (req, res, next) => {
+  const data = { ...req.body, EdificioId: Number(req.params.id) };
+  Dependencias.create(data)
+    .then(dependencia => {
+      res.status(201).json(dependencia);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = depenRouter;
--- api/routes/edificios.js
+++ api/routes/edificios.js
@@ -2,12 +2,26 @@ const express = require("express");
 const edificiosRouter = express.Router();
 const { Edificios } = require("../models");
 
+// traer todos los edificios
 edificiosRouter.get("/", (req, res, next) => {
   Edificios.findAll()
     .then(edificios => {
       res.status(200).json(edificios);
     })
-    .catch(next);
+    .catch(err => {
+      next(err);
+    });
+});
+
+// crear un nuevo edificio
+edificiosRouter.put("/", (req, res, next) => {
+  Edificios.create(req.body)
+    .then(edificio => {
+      res.status(201).json(edificio);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = edificiosRouter;
